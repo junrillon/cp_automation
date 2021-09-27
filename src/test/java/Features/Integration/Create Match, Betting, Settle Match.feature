351bf81ad9074ff630fd4f15de76a821a4558738
jerald <jeraldmanamtam12@gmail.com>
2021-09-27 15:18:53 +0800

    Integration feature update 9/27 (Jerald)

---
+++ src/test/java/Features/Integration/Create Match, Betting, Settle Match.feature
@@ -0,0 +1,35 @@
+# new feature
+# Tags: optional
+Feature: Admin Create match and player betting
+
+  @CreateMatch
+  Scenario: go to create match
+    Given i can access admin backoffice login page https://admin.cpp555.com/login
+    When i input the admin Username andre and Password 123123
+    And i click the login button
+    Then i can access the homepage
+    And i click the games header dropdown
+    And i click matches
+    And i click the create match button
+    And i input match details
+      | Sport | League      | Match count |
+      | Test  | Test League | 1           |
+    And i click submit button
+    And i select view match details from action dropdown
+    Then i click open bet status
+
+  @PlayerBetting
+  Scenario: Select sports
+    Given i can access frontend login page https://www.es3838.com/login/
+    When i input the Username jeraldnew and Password 123123
+    And i click the login button to access the betting page
+    Then i can access the betting page
+    And i click the pool header button
+    And i click the available sports
+    And i select team and input bet amount
+        #Selection 1 = TEAM A, 2 = TEAM B, 3 = DRAW
+      |Selection |Amount|
+      |1         |10    |
+    And i click place bet button
+    And i confirm my place bet
+    Then place bet success
---
+++ src/test/java/Pages/Frontend/MatchDetails.java
@@ -0,0 +1,67 @@
+package Pages.Frontend;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class MatchDetails {
+    public MatchDetails(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    /**
+     * Object repository
+     */
+    //TEAM A
+    @FindBy(how = How.ID, using =  "submit-bet-placeBetTeam1")
+    public WebElement selectionA;
+
+    //TEAM B
+    @FindBy(how = How.ID, using =  "submit-bet-placeBetTeam2")
+    public WebElement selectionB;
+
+    //DRAW
+    @FindBy(how = How.ID, using =  "submit-bet-placeBetDraw")
+    public WebElement selectionDraw;
+
+    //input amount
+    @FindBy(how = How.CSS, using =  "input.form-control")
+    public WebElement amountInput;
+
+    //plce bet button
+    @FindBy(how = How.XPATH, using =  "(//button[@type='button'])[6]")
+    public WebElement clickSubmitBtn;
+
+    //confirm button
+    @FindBy(how = How.XPATH, using =  "(//button[@type='button'])[8]")
+    public WebElement confirmPlacebet;
+
+
+
+
+    /**
+     * Object action
+     */
+    //Click TEAM A
+    public void ClickTeamA() {selectionA.isDisplayed(); selectionA.click();}
+
+    //click TEAM B
+    public void ClickTeamB() {selectionB.click();}
+
+    //click pool header button
+    public void ClickDraw() {selectionDraw.click();}
+
+    //click pool header button
+    public void inputAmount(String matchCountInput) {amountInput.isDisplayed(); amountInput.sendKeys(matchCountInput);}
+
+    //click pool header button
+    public void clickPlaceBetBtn() {clickSubmitBtn.click();}
+
+    //click pool header button
+    public void clickConfirmPlaceBetBtn() {confirmPlacebet.isDisplayed(); confirmPlacebet.click();}
+
+
+}
