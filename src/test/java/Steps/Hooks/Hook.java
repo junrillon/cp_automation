0d75d4152d89575ac4bb554f79cfb5d25e7669e8
junarson rillon <junarson.rillon>
2022-02-16 15:51:41 +0800

    1. Deleted generated files

--- src/test/java/Steps/Hooks/Hook.java
+++ src/test/java/Steps/Hooks/Hook.java
@@ -29,7 +29,7 @@ public class Hook extends BaseUtil {
         options.addArguments("--remote-debugging-port=9223");
         options.addArguments("--start-maximized");
         //options.addArguments("--auto-open-devtools-for-tabs");
-        WebDriver browser = new ChromeDriver(options);
+        //WebDriver browser = new ChromeDriver(options);
         options.setExperimentalOption("debuggerAddress", "127.0.0.1:9223");
         base.Driver = new ChromeDriver(options);
     }
