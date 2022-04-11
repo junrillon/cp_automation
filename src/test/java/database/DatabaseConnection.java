d722f2cbacee871fb319c5442e2a0d2186507b4f
junarson rillon <junarson.rillon>
2022-04-11 17:14:32 +0800

    1. Change database credentials.

--- src/test/java/database/DatabaseConnection.java
+++ src/test/java/database/DatabaseConnection.java
@@ -9,14 +9,12 @@ public class DatabaseConnection {
         if (conn != null && !conn.isClosed()) {
             conn.close();
         }
-        conn = DriverManager.getConnection("jdbc:mysql://54.65.174.214:3306/stage_pool_betting", "stage_jun", "#3s9VrD$P!Gg-cda");
+        conn = DriverManager.getConnection("jdbc:mysql://54.65.174.214:3306/stage_pool_betting", "qa_autobot", "eb%AqG-qvumD96Qe");
         Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
         ResultSet rs = stmt.executeQuery(queryString);
         rs.next();
         return rs;
 
-        //54.65.174.214 <- b2c staging "stage_jun", "#3s9VrD$P!Gg-cda"
-        //18.140.122.219:3306 <- dev pool bet "stage.jun.rillon", "hEUN5dI8UN5dI894s");
     }
     public void closeConn() throws SQLException {
         if(conn !=null && !conn.isClosed()) {
