c0b3adb3ba8b9fa4f6f08b166fb7826f01d4f7dc
junarson rillon <junarson.rillon>
2024-02-20 16:01:06 +0800

    1. Move directory

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,20 +1,18 @@
 package steps;
 
-import atu.testrecorder.ATUTestRecorder;
+//import atu.testrecorder.ATUTestRecorder;
+
 import engine.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Collection;
-import java.util.Date;
 
 public class Hooks {
 
     private final Driver driver;
-    ATUTestRecorder recorder;
+   // ATUTestRecorder recorder;
     public static String sce;
 
     public Hooks(Driver driver) {
@@ -26,31 +24,31 @@ public class Hooks {
         Collection<String> tags = scenario.getSourceTagNames();
         String scenarioName = scenario.getName();
 
-        if (tags.contains("@Record")) {
-            System.out.println("Starting execution of test case with recording: " + scenarioName);
-            DateFormat dateFormat = new SimpleDateFormat("yy-MM-dd HH-mm-ss");
-            Date date = new Date();
-
-            try {
-                recorder = new ATUTestRecorder(System.getProperty("user.dir") + "\\scriptVideos\\",
-                        scenarioName + "-" + dateFormat.format(date), false);
-            } catch (Exception e) {
-                System.out.println("Error in finding the location of the video.");
-            }
-
-            try {
-                recorder.start();
-            } catch (Exception e){
-                System.out.println("Error in starting the video.");
-            }
-
-            driver.start();
-        } else {
-            System.out.println("Starting execution of test case without recording: " + scenarioName);
-        }
-
-        // Additional setup code for scenarios with and without the @Record tag
-        // ...
+//        if (tags.contains("@Record")) {
+//            System.out.println("Starting execution of test case with recording: " + scenarioName);
+//            DateFormat dateFormat = new SimpleDateFormat("yy-MM-dd HH-mm-ss");
+//            Date date = new Date();
+//
+//            try {
+//                recorder = new ATUTestRecorder(System.getProperty("user.dir") + "\\scriptVideos\\",
+//                        scenarioName + "-" + dateFormat.format(date), false);
+//            } catch (Exception e) {
+//                System.out.println("Error in finding the location of the video.");
+//            }
+//
+//            try {
+//                recorder.start();
+//            } catch (Exception e){
+//                System.out.println("Error in starting the video.");
+//            }
+//
+//            driver.start();
+//        } else {
+//            System.out.println("Starting execution of test case without recording: " + scenarioName);
+//        }
+//
+//        // Additional setup code for scenarios with and without the @Record tag
+//        // ...
 
         driver.start();
     }
@@ -58,14 +56,14 @@ public class Hooks {
    @After()
     public void after(Scenario scenario){
        Collection<String> tags = scenario.getSourceTagNames();
-        if(tags.contains("@Record")) {
-            try {
-                recorder.stop();
-
-            } catch (Exception e) {
-                System.out.println("Unable to stop the screen recording.");
-            }
-        }
+//        if(tags.contains("@Record")) {
+//            try {
+//                recorder.stop();
+//
+//            } catch (Exception e) {
+//                System.out.println("Unable to stop the screen recording.");
+//            }
+//        }
 
        driver.close();
    }
